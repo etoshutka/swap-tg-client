890eb714f45d3d164fe789ec1e3ba7e4a1318979
etoshutka <cheatmaster114@gmail.com>
2024-09-18 13:02:29 +0300

    API test

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,11 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     domains: ['s2.coinmarketcap.com', 'cache.tonapi.io', 'crypto-swap-test.ru', 'www.crypto-swap-test.ru'],
+    unoptimized: true,
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/swap-tg-client' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/swap-tg-client' : '',
+  output: 'export',
 };
 
 export default nextConfig;
